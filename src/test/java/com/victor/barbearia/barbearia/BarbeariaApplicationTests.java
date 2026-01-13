fddf571d747ca0a89ed2979820b04ef1ed6dda8d
VictorOliveira <joaouns@gmail.com>
2026-01-13 10:47:24 -0300

    feat: projeto inicial Spring Boot

---
+++ src/test/java/com/victor/barbearia/barbearia/BarbeariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.barbearia.barbearia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
